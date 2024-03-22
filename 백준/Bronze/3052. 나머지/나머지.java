4bba0eba52d8e76fa9563bb3495218db8bea7f30
githublees <44081552+githublees@users.noreply.github.com>
2024-03-22 17:10:41 +0900

    [Bronze II] Title: 나머지, Time: 120 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.HashSet;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		HashSet<Integer> set = new HashSet<>();
+		
+		for(int i=1; i<=10; i++) {
+			set.add(Integer.parseInt(br.readLine()) % 42);
+		}
+		
+		System.out.println(set.size());
+	}
+}
