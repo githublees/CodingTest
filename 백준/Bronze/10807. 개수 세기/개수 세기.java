245a35645e08d4beb4fe35cd3df486764ade89b3
githublees <44081552+githublees@users.noreply.github.com>
2024-03-22 11:34:00 +0900

    [Bronze V] Title: 개수 세기, Time: 128 ms, Memory: 14296 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -1,20 +1,25 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int[] v = new int[201];
 		
-		int N = Integer.parseInt(sc.next());
+		int N = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i=0; i<N; i++) {
-			int x = Integer.parseInt(sc.next());
+			int x = Integer.parseInt(st.nextToken());
 			
 			v[x+100]++;
 		}
 		
-		int find = Integer.parseInt(sc.next());
+		int find = Integer.parseInt(br.readLine());
 		
 		System.out.println(v[find+100]);
 	}
