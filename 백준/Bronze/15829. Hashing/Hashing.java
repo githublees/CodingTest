7c76adda23a498b5066fd8634ed73188bde8a3c6
githublees <44081552+githublees@users.noreply.github.com>
2024-03-23 17:22:14 +0900

    [Bronze II] Title: Hashing, Time: 124 ms, Memory: 14208 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -5,12 +5,18 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int hash = 0;
+		long hash = 0;
 		int l = Integer.parseInt(br.readLine());
 		char[] ch = br.readLine().toCharArray();
 		
 		for(int i=0; i<l; i++) {
-			hash += (ch[i] - 96) * Math.pow((double)31, i) % 1234567891;
+			long r = 1;
+			
+			for(int j=1; j<=i; j++) {
+				r = ((r  * 31) % 1234567891);
+			}
+			
+			hash = ((hash % 1234567891) + ((ch[i]- 96) * r ) % 1234567891 ) % 1234567891;
 		}
 		
 		System.out.println(hash);
