ee42b82f67549143ae16f227d5f9327af560a274
githublees <44081552+githublees@users.noreply.github.com>
2024-03-24 13:48:37 +0900

    [Bronze II] Title: 벌집, Time: 124 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.java
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -5,16 +5,12 @@ public class Main {
 	public static void main(String[] args) throws IOException {		
 		
 		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-		
+
 		int r = Integer.parseInt(br.readLine());
 		
-		int depth = 0; 
-		while(r > 0) {
-			if(depth == 0) r -= 1;
-			else {
-				r = r - depth * 6;
-			}
-			depth++;
+		int depth = 1; 
+		while(r > 1) {
+			r -= depth++ * 6;
 		}
 		
 		System.out.println(depth);
