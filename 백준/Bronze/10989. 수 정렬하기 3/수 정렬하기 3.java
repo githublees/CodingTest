67a60310c000b95f11955c3f20b132dc2c6343c6
githublees <44081552+githublees@users.noreply.github.com>
2024-03-26 18:34:10 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1812 ms, Memory: 339008 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -14,7 +14,7 @@ public class Main {
 			num[i] = Integer.parseInt(br.readLine());
 		}
 		
-//		quickSort(num, 0, num.length-1);
+//		quickSort(0, num.length-1);
 		countingSort();
 		
 		StringBuilder sb = new StringBuilder();
@@ -28,14 +28,14 @@ public class Main {
 	/*
 	 *  버블 정렬 O(n^2)
 	 */
-	public static void bubbleSort(int[] arr) {
+	public static void bubbleSort() {
 		int temp = 0;
-		for(int i=0; i< arr.length; i++) {
-			for(int j=1; j<arr.length-i; j++) {
-				if(arr[j-1] > arr[j]) {
-					temp = arr[j-1];
-					arr[j-1] = arr[j];
-					arr[j] = temp;
+		for(int i=0; i< num.length; i++) {
+			for(int j=1; j<num.length-i; j++) {
+				if(num[j-1] > num[j]) {
+					temp = num[j-1];
+					num[j-1] = num[j];
+					num[j] = temp;
 				}
 			}
 		}
@@ -44,60 +44,60 @@ public class Main {
 	/*
 	 *  삽입 정렬 O(N), 최악의 경우 O(n^2)
 	 */
-	public static void insertionSort(int[] arr) {
-		for(int i=1; i<arr.length; i++) {
-			int temp = arr[i];
+	public static void insertionSort() {
+		for(int i=1; i<num.length; i++) {
+			int temp = num[i];
 			int prev = i - 1;
-			while((prev >= 0) && (arr[prev] > temp)) {
-				arr[prev+1] = arr[prev];
+			while((prev >= 0) && (num[prev] > temp)) {
+				num[prev+1] = num[prev];
 				prev--;
 			}
-			arr[prev+1] = temp;
+			num[prev+1] = temp;
 		}
 	}
 	
 	/*
 	 *  퀵 정렬 O(nlogn), 최악의 경우 O(n^2)
 	 */
-	public static void quickSort(int[] arr, int left, int right) {
+	public static void quickSort(int left, int right) {
 		if(left >= right) return;
 		
-		int pivot = partitions(arr, left, right);
+		int pivot = partitions(left, right);
 		
-		quickSort(arr, left, pivot-1);
-		quickSort(arr, pivot+1, right);
+		quickSort(left, pivot-1);
+		quickSort(pivot+1, right);
 	}
 	
-	private static int partitions(int[] arr, int left, int right) {
+	private static int partitions(int left, int right) {
 
 		int mid = (left + right) / 2;
-		swap(arr, left, mid);
+		swap(left, mid);
 
-		int pivot = arr[left]; // 가장 왼쪽값을 피벗으로 설정
+		int pivot = num[left]; // 가장 왼쪽값을 피벗으로 설정
 		int i = left, j = right;
 		
 		while(i < j) {
 			
 			// 오른쪽에서부터 pivot 보다 작은 값을 찾는다.
-			while(pivot < arr[j]) j--;
+			while(pivot < num[j]) j--;
 			
 			// 왼쪽에서부터 pivot 보다 큰 값을 찾으면서 i 가 j 보다 작으면 i++
-			while (i < j && pivot >= arr[i]) i++;
+			while (i < j && pivot >= num[i]) i++;
 			
-			swap(arr, i, j);
+			swap(i, j);
 		}
 		// pivot인 왼쪽값에 현재 i값을 넣고 i값에는 pivot으로 설정했던 왼쪽값을 넣는다.
-		arr[left] = arr[i];
-		arr[i] = pivot;
+		num[left] = num[i];
+		num[i] = pivot;
 		
 		// i를 기준으로 파티션을 나누도록 i 반환
 		return i;
 	}
 	
-	private static void swap(int[] arr, int i, int j) {
-		int temp = arr[i];
-		arr[i] = arr[j];
-		arr[j] = temp;
+	private static void swap(int i, int j) {
+		int temp = num[i];
+		num[i] = num[j];
+		num[j] = temp;
 	}
 
 	/*
@@ -105,7 +105,7 @@ public class Main {
 	 * 최선의 경우 O(nlogn)의 시간 복잡도, 최악의 경우 O(n)의 시간복잡도
 	 */
 	public static void countingSort() {
-		int[] sorted_arr = new int[num.length];
+		int[] sorted_num = new int[num.length];
 		
 		// 1) counting 배열의 사이즈를 최대값이 담기도록 크게 잡기
 		// 때문에 메모리 낭비가 심하다.
@@ -124,9 +124,9 @@ public class Main {
 		// 4) 뒤에서부터 배열을 돌면서, 해당하는 값의 인덱스에 값을 넣어주기
 		for(int i=num.length-1; i>=0; i--) {
 			counting[num[i]]--;
-			sorted_arr[counting[num[i]]] = num[i];
+			sorted_num[counting[num[i]]] = num[i];
 		}
 		
-		num = sorted_arr;
+		num = sorted_num;
 	}
 }
