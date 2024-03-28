560e2413df92f4eb3a7903c8fb5bf1204312e348
githublees <44081552+githublees@users.noreply.github.com>
2024-03-28 13:25:00 +0900

    [Gold V] Title: 암호 만들기, Time: 132 ms, Memory: 14372 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -13,6 +13,7 @@ public class Main {
 		st = new StringTokenizer(br.readLine());
 		
 		ArrayList<String> alphabet = new ArrayList<>();
+		
 		for(int i=0; i<C; i++) {
 			alphabet.add(st.nextToken());
 		}
@@ -26,27 +27,30 @@ public class Main {
 	
 	private static void combination(ArrayList<String> alphabet, StringBuilder pwd, int start, int l, int c, int r) {
 		if(l == r) {
-			
-			int vo = 0;
-			for(int i=0; i<r; i++) {
-				if(pwd.charAt(i) == 'a' || pwd.charAt(i) == 'e' || pwd.charAt(i) == 'i' || pwd.charAt(i) == 'o' || pwd.charAt(i) == 'u') {
-					vo++;
-				}
-			}
-			
-			if(vo >= 1 && pwd.length() - vo >= 2) {
+			if(vowels_consonant_check(pwd, 0, 0)) { 
 				System.out.println(pwd.toString());
 			}
-			
 			return;
 		}
 		
 		for(int i=start; i<c; i++) {
-			
 			pwd.append(alphabet.get(i));
 			combination(alphabet, pwd, i+1, l, c,r+1);
 			pwd.deleteCharAt(pwd.length()-1);
 		}
 	}
-
+	
+	private static boolean vowels_consonant_check(StringBuilder sb, int vo, int co) {
+		for(int i=0; i<sb.length(); i++) {
+			if(sb.charAt(i) == 'a' || sb.charAt(i) == 'e' || sb.charAt(i) == 'i' 
+				|| sb.charAt(i) == 'o' || sb.charAt(i) == 'u') {
+				vo++;
+			}
+		}
+		
+		if(vo >= 1 && sb.length() - vo >= 2) {
+			return true;			
+		}
+		return false;
+	}
 }
