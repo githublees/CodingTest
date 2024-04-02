dce3a1dd1aaab96b20c634d194d257dcc70a63e8
githublees <44081552+githublees@users.noreply.github.com>
2024-04-02 10:30:45 +0900

    [Gold V] Title: A와 B 2, Time: 124 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Gold/12919. A와 B 2/A와 B 2.java
+++ 백준/Gold/12919. A와 B 2/A와 B 2.java
@@ -1,44 +1,47 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Deque;
-import java.util.LinkedList;
+import java.io.*;
 
 public class Main {
-	
-	static int isFlag;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		String S = br.readLine();
 		String T = br.readLine();
-		isFlag = 0;
 		
-		change(new StringBuilder(S), new StringBuilder(T));
-		
-		System.out.println(isFlag);
+		System.out.println(dfs(S, new StringBuilder(T)) ? 1 : 0);
 	}
 	
-	private static void change(StringBuilder s, StringBuilder t) {
+	private static boolean dfs(String s, StringBuilder t) {
 	
 		if(s.length() == t.length()) {
-			if(t.toString().equals(s.toString())) {
-				isFlag = 1;				
+			
+			if(t.toString().equals(s)) {
+				return true;				
 			}
-			return;
+			
+			return false;
 		}
 
 		if(t.charAt(t.length()-1) == 'A' ) {
+			
 			t.deleteCharAt(t.length()-1);
-			change(new StringBuilder(s), new StringBuilder(t));
+			
+			if(dfs(s, new StringBuilder(t))) {
+				return true;
+			}
+			
 			t.append("A");	
 		}		
 		
 		if(t.charAt(0) == 'B') {
+			
 			t.reverse().deleteCharAt(t.length()-1);
-			change(new StringBuilder(s), new StringBuilder(t));
-			t.append("B").reverse();
+			
+			if(dfs(s, new StringBuilder(t))) {
+				return true;
+			}
 		}
+		
+		return false;
 	}
 }
