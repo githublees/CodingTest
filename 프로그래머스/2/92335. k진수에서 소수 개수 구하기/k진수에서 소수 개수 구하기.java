ad2c527cd48abd19d67d5cebba3c103c97cad97f
Hoseong Lee <44081552+githublees@users.noreply.github.com>
2024-04-05 12:32:14 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.18 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int k) {
+        int answer = 0;
+        
+        StringBuilder sb = new StringBuilder();
+       
+        while(n > k) {
+            sb.append(n % k);
+            n /= k;
+        }
+        sb.append(n).reverse();
+        
+        String[] prime = sb.toString().split("0");
+        ArrayList<Long> p_list = new ArrayList<>();
+        
+        for(int i=0; i<prime.length; i++) {
+            if(prime.equals(" ") || prime[i].equals("") || Long.parseLong(prime[i]) == 1) continue;
+            p_list.add(Long.parseLong(prime[i]));
+        }
+        
+        for(int i=0; i<p_list.size(); i++) {
+            
+            long p = p_list.get(i);
+            
+            boolean isPrime = true;
+            for(int j=2; j<=Math.sqrt(p); j++) {
+                if(p%j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+           
+            
+            if(isPrime) answer++;
+        }
+        
+        return answer;
+    }
+}
