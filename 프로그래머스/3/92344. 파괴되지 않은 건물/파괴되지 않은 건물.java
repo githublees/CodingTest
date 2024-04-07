d9e800dac9c6f9c66747497eebe8392db8327560
Hoseong Lee <44081552+githublees@users.noreply.github.com>
2024-04-08 04:20:47 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 88.13 ms, Memory: 213 MB -BaekjoonHub

---
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int solution(int[][] board, int[][] skill) {
+        int answer = 0;
+        
+        int N = board.length;
+        int M = board[0].length;
+ 
+        int[][] sum = new int[N + 1][M + 1];
+        for (int[] s : skill) {
+            int y1 = s[1], x1 = s[2];
+            int y2 = s[3], x2 = s[4];
+            int degree = s[5] * (s[0] == 1 ? -1 : 1);
+ 
+            sum[y1][x1] += degree;
+            sum[y1][x2 + 1] += (degree * -1);
+            sum[y2 + 1][x1] += (degree * -1);
+            sum[y2 + 1][x2 + 1] += degree;
+        }
+        
+        for (int y = 1; y < N; y++) {
+            for (int x = 0; x < M; x++) {
+                sum[y][x] += sum[y - 1][x];
+            }
+        }
+        
+        for (int x = 1; x < M; x++) {
+            for (int y = 0; y < N; y++) {
+                sum[y][x] += sum[y][x - 1];
+            }
+        }
+ 
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (board[i][j] + sum[i][j] > 0) answer++;
+            }
+        }
+        return answer;
+    }
+}
