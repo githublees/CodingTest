748eb18e7a189036b3d5ccb933be15cc5fb3a039
Hoseong Lee <44081552+githublees@users.noreply.github.com>
2024-04-28 15:38:06 +0900

    [Gold V] Title: 센서, Time: 216 ms, Memory: 16412 KB -BaekjoonHub

--- 백준/Gold/2212. 센서/센서.java
+++ 백준/Gold/2212. 센서/센서.java
@@ -1,37 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
+import java.util.stream.Collectors;
+
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		//1. 입력을 받는다.
-		int n = sc.nextInt(); // 센서의 갯수
-		int k = sc.nextInt(); // 집중국의 갯수
-		//2. 만약 집중국 갯수(k) >= 센서 갯수(n)이면, 0을 출력하고 종료한다.
-		if(k >= n) {
-			System.out.println(0);
-			return;
-		}
-		int sum = 0;
-		
-		//1. 입력을 받는다
-		int[] sensorArr = new int[n];
-		for(int i = 0; i < n; i++)
-			sensorArr[i] = sc.nextInt();
-		
-		//3. 센서의 거리를 오름차순으로 정렬한다. (순서대로 배치)
-		Arrays.sort(sensorArr);
-		
-		//4. 각 센서 거리의 차이를 담은 배열을 만든다.
-		Integer[] diffArr = new Integer[n-1];
-		for(int i = 0; i < n-1; i++)
-			diffArr[i] = sensorArr[i+1] - sensorArr[i];
-		
-		//5. 차이 배열을 내림차순으로 정렬한다.
-		Arrays.sort(diffArr, Collections.reverseOrder());
-		
-		//6. 차이 배열의 k-1 ~ 마지막 까지의 합을 출력한다.
-		for(int i = k-1; i < n-1; i++) {
-			sum += diffArr[i];
-		}
-		System.out.println(sum);
-	}
+
+    static int max = Integer.MIN_VALUE;
+    static int min = Integer.MAX_VALUE;
+
+    static int[] sensor;
+    static int N;
+    static int K;
+
+    static int answer;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        K = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        sensor = new int[N];
+        for (int i = 0; i < N; i++) {
+            sensor[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(sensor);
+
+        Integer[] dis = new Integer[N-1];
+        for (int i = 0; i < N-1; i++) {
+            dis[i] = sensor[i+1] - sensor[i];
+        }
+
+        Arrays.sort(dis, Comparator.reverseOrder());
+
+        for (int i = K-1; i < N-1; i++) {
+            answer += dis[i];
+        }
+
+        System.out.println(answer);
+    }
 }
