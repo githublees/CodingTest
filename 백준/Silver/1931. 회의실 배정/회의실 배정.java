df759c6e95ea5b9a6d7a26223a25635fc01263ea
Hoseong Lee <44081552+githublees@users.noreply.github.com>
2024-08-23 12:02:19 +0900

    [Silver I] Title: 회의실 배정, Time: 428 ms, Memory: 41744 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static class Conference implements Comparable<Conference>{
+        int start;
+        int end;
+
+        public Conference(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
+
+        @Override
+        public int compareTo(Conference o) {
+            if (this.end == o.end) {
+                return this.start - o.start;
+            }
+            return this.end - o.end;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Conference> pq = new PriorityQueue<>();
+
+        while (N --> 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+
+            pq.offer(new Conference(start, end));
+        }
+
+        int min = 0, answer = 0;
+
+        while (!pq.isEmpty()) {
+            Conference c = pq.poll();
+
+            if (min <= c.start) {
+                answer++;
+                min = c.end;
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
