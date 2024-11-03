6fa32bd7ae2391c68f89fa518a1eea93b573e4d6
Hoseong Lee <44081552+githublees@users.noreply.github.com>
2024-11-03 19:05:44 +0900

    [Gold V] Title: 물병, Time: 100 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Gold/1052. 물병/물병.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N, K;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        int answer = 0;
+
+        while (Integer.bitCount(N) > K) {
+            answer += N & (-N);
+            N += N & (-N);
+        }
+
+        System.out.println(answer);
+    }
+}
