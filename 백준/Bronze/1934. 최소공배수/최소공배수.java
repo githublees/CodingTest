1f228c3e876dc7c61f4f8f9adb6dec0fd50ddfe3
Hoseong Lee <44081552+githublees@users.noreply.github.com>
2024-11-16 22:01:24 +0900

    [Bronze I] Title: 최소공배수, Time: 144 ms, Memory: 15140 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int testcase = 0; testcase < T; testcase++) {
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            System.out.println(A * B / gcd(A, B));
+        }
+    }
+
+    private static int gcd(int a, int b) {
+        if (a % b == 0) {
+            return b;
+        }
+        return gcd(b, a % b);
+    }
+}
