8da6bbfd96a13908075625d405e3a7f085e7183b
Hoseong Lee <44081552+githublees@users.noreply.github.com>
2025-01-01 13:44:54 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 684 ms, Memory: 105176 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
@@ -1,47 +1,37 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static char[] S;
+    static String S;
     static int q;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
 
-        Map<Character, int[]> map = new HashMap<>();
+        S = br.readLine();
+        q = Integer.parseInt(br.readLine());
+        int[][] sum = new int[26][S.length() + 1];
 
-        S = br.readLine().toCharArray();
-        for (char i = 'a'; i <= 'z'; i++) {
-
-            int[] sum = new int[S.length + 1];
-
-            for (int j = 1; j <= S.length; j++) {
-
-                if (S[j-1] == i) {
-                    sum[j] = sum[j-1] + 1;
-                } else {
-                    sum[j] = sum[j-1];
-                }
+        for (int i = 0; i < 26; i++) {
+            for (int j = 1; j <= S.length(); j++) {
+                sum[i][j] = S.charAt(j-1) - 'a' == i ? sum[i][j-1] + 1 : sum[i][j-1];
             }
-
-            map.put(i, sum);
         }
 
-        q = Integer.parseInt(br.readLine());
         for (int i = 0; i < q; i++) {
             st = new StringTokenizer(br.readLine());
-            String alphabet = st.nextToken();
+            int alphabet = st.nextToken().charAt(0) - 'a';
             int l = Integer.parseInt(st.nextToken());
             int r = Integer.parseInt(st.nextToken());
 
-            int[] sum = map.get(alphabet.charAt(0));
-            System.out.println(sum[r+1] - sum[l]);
+            sb.append(sum[alphabet][r+1] - sum[alphabet][l]).append("\n");
         }
+
+        System.out.println(sb);
     }
 }
