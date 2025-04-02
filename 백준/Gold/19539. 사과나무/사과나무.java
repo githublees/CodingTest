2e51799d7373df7daf5c96f9983c216a6862e92c
HoSeong Lee <44081552+githublees@users.noreply.github.com>
2025-04-02 20:11:36 +0900

    [Gold V] Title: 사과나무, Time: 248 ms, Memory: 23336 KB -BaekjoonHub

---
+++ 백준/Gold/19539. 사과나무/사과나무.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int sum = 0;
+        int one = 0;
+        int two = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            sum += num;
+            one += num % 2;
+            two += num / 2;
+        }
+
+        if (sum % 3 > 0 || one > two) {
+            System.out.println("NO");
+            return;
+        }
+        System.out.println("YES");
+    }
+}
