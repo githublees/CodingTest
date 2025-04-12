57932f5e75b404ab8a191f5688d681a3e68e5913
HoSeong Lee <44081552+githublees@users.noreply.github.com>
2025-04-12 13:57:24 +0900

    [Silver I] Title: 1, 2, 3 더하기 9, Time: 168 ms, Memory: 24552 KB -BaekjoonHub

---
+++ 백준/Silver/16195. 1， 2， 3 더하기 9/1， 2， 3 더하기 9.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static long[][] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        init();
+
+        int T = Integer.parseInt(br.readLine());
+        for (int testcase = 0; testcase < T; testcase++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+            
+            long sum = 0;
+            for (int i = 0; i <= m; i++) {
+                sum = (sum + dp[n][i]) % 1_000_000_009;
+            }
+            sb.append(sum).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    private static void init() {
+        dp = new long[1001][1001];
+        dp[1][1] = 1;
+        dp[2][1] = 1;
+        dp[2][2] = 1;
+        dp[3][1] = 1;
+        dp[3][2] = 2;
+        dp[3][3] = 1;
+        for (int i = 4; i < 1001; i++) {
+            for (int j = 2; j < 1001; j++) {
+                dp[i][j] = (dp[i-1][j-1] + dp[i-2][j-1] + dp[i-3][j-1]) % 1_000_000_009;
+            }
+            dp[i][i] = 1;
+        }
+    }
+}
